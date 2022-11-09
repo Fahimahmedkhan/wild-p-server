65b1868b822fffb631ac44b0d7affe6ed05e693c
fahimahmedkhan <rolx9999@gmail.com>
2022-11-09 14:05:12 +0600

    reviews

--- index.js
+++ index.js
@@ -62,15 +62,28 @@ async function run() {
         // orders api
         app.get('/reviewCollection', async (req, res) => {
             let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
+
         app.post('/reviewCollection', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         });
+
+        app.delete('/reviewCollection/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
 
         app.post('/myPhotoCollection', async (req, res) => {
             const photo = req.body;
